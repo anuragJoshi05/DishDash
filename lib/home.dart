37dc350b2910dcab2ab87480f8922f58abdfd31f
anuragJoshi05 <anurag88787@gmail.com>
2024-05-25 16:22:47 +0530

    feat: Add recipe categories and improve UI
    - Introduced a new horizontal scrolling list of recipe categories with images and headings.
    - Improved styling for recipe cards and category cards, ensuring proper clipping and rounded corners.
    - Refactored the `RecipeModel` class and cleaned up the `getRecipe` function for better readability and performance.
    - Enhanced user interaction by enabling recipe search via the search icon and text submission.
    - Ensured consistent gradient background across the app by setting `Card` backgrounds to transparent.
    - Added a `SafeArea` widget to keep the layout within the visible screen area.

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,23 @@
 import 'dart:convert';
-import 'package:dishdash/model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+class RecipeModel {
+  final String appLabel;
+  final String appImgUrl;
+  final double appCal;
+
+  RecipeModel(
+      {required this.appLabel, required this.appImgUrl, required this.appCal});
+
+  factory RecipeModel.fromMap(Map<String, dynamic> map) {
+    return RecipeModel(
+      appLabel: map['label'],
+      appImgUrl: map['image'],
+      appCal: map['calories'],
+    );
+  }
+}
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,6 +29,33 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List<RecipeModel> recipeList = <RecipeModel>[];
   TextEditingController searchController = TextEditingController();
+  List recipeCatList = [
+    {
+      "imgUrl":
+      "https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3BpY3klMjBmb29kfGVufDB8fDB8fHww",
+      "heading": "Chilli Food"
+    },
+    {
+      "imgUrl":
+      "https://images.unsplash.com/photo-1541599188778-cdc73298e8fd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGRlc3NlcnR8ZW58MHx8MHx8fDA%3D",
+      "heading": "Deserts"
+    },
+    {
+      "imgUrl":
+      "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "heading": "Diet Food"
+    },
+    {
+      "imgUrl":
+      "https://cdn.pixabay.com/photo/2020/03/21/04/00/shrimp-4952607_1280.jpg",
+      "heading": "Sea Food"
+    },
+    {
+      "imgUrl":
+      "https://media.istockphoto.com/id/184946701/photo/pizza.jpg?s=612x612&w=0&k=20&c=97rc0VIi-s3mn4xe4xDy9S-XJ_Ohbn92XaEMaiID_eY=",
+      "heading": "Pizza"
+    },
+  ];
 
   Future<void> getRecipe(String query) async {
     String url =
@@ -24,10 +67,6 @@ class _HomeState extends State<Home> {
       RecipeModel recipeMod = RecipeModel.fromMap(element["recipe"]);
       recipeList.add(recipeMod);
     });
-    for (var recipe in recipeList) {
-      print(recipe.appLabel);
-      print(recipe.appImgUrl); // Print image URLs to verify
-    }
     setState(() {}); // To refresh the UI with new data
   }
 
@@ -143,7 +182,7 @@ class _HomeState extends State<Home> {
                                     .cover, // Ensures the image covers the entire area
                                 width: double.infinity,
                                 height:
-                                    200.0, // You can adjust the height as needed
+                                200.0, // You can adjust the height as needed
                               ),
                             ),
                             Positioned(
@@ -175,7 +214,7 @@ class _HomeState extends State<Home> {
                                   child: Center(
                                     child: Row(
                                       mainAxisAlignment:
-                                          MainAxisAlignment.center,
+                                      MainAxisAlignment.center,
                                       children: [
                                         const Icon(Icons.local_fire_department),
                                         Text(
@@ -187,13 +226,69 @@ class _HomeState extends State<Home> {
                                     ),
                                   ),
                                 )
-                                // Add other widget elements for your card here if needed
-                                ),
+                              // Add other widget elements for your card here if needed
+                            ),
                           ],
                         ),
                       ),
                     );
                   },
+                ),
+                SizedBox(
+                  height: 250, // Provide a fixed height for the ListView
+                  child: ListView.builder(
+                    itemCount: recipeCatList.length,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.all(20),
+                        child: InkWell(
+                          onTap: () {},
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            elevation: 0.00,
+
+                            child: Stack(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Image.network(
+                                    recipeCatList[index]["imgUrl"],
+                                    fit: BoxFit.cover,
+                                    width: 200,
+                                    height: 250,
+                                  ),
+                                ),
+                                Positioned(
+                                  left: 0,
+                                  right: 0,
+                                  bottom: 0,
+                                  top: 0,
+                                  child: Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 5, horizontal: 10),
+                                    decoration: const BoxDecoration(
+                                        color: Colors.black26),
+                                    child: Center(
+                                      child: Text(
+                                        recipeCatList[index]["heading"],
+                                        style: const TextStyle(
+                                            color: Colors.white, fontSize: 28),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ),
               ],
             ),
